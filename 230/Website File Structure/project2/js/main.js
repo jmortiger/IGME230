1b0ea9b2a24e9a6c71047bf7b5b9b27ecd71d019
Justin Morris <jmortiger@gmail.com>
2019-04-09 16:26:59 -0400

    d

--- 230/Website File Structure/project2/js/main.js
+++ 230/Website File Structure/project2/js/main.js
@@ -95,6 +95,10 @@ function searchByBreedAll() {
     let url = SERVICE_URL + "/" + userInput + "/images";
     getData(url);
 }
+// ****************************TODO: THIS*************************************************
+function showFavorites() {
+
+}
 
 // Tries to reformat multi-word breeds into applicable formats (i.e. "golden retriever" -> "retriever-golden")
 function formatUserString(userInput) {
@@ -165,7 +169,7 @@ function jsonLoaded(obj) {
     let favButtons = document.querySelectorAll(".favButton");
     for (let i = 0; i < favButtons.length; i++) {
         // if the current list of favs has this url
-        if (favs.find(function(elem) { return elem == favButtons[i].getAttribute("imgURL"); }) != undefined) {
+        if (favs.find(function (elem) { return elem == getImgURLAttrib(favButtons[i])/*favButtons[i].getAttribute("imgURL")*/; }) != undefined) {
             favButtons[i].innerHTML = "Favorited!";// TODO: CHANGE CSS TO MAKE IT CLEAR IT'S ALREADY BEEN FAVORITED AND CLICKING WILL UNFAVORITE
             favButtons[i].onclick = removeFavorite;
         }
@@ -178,7 +182,7 @@ function jsonLoaded(obj) {
 function addFavorite(e) {
     console.log(`addFavorite called`);
     console.log(`length before: ${favs.length}`);
-    debugger;
+    //debugger;
     favs.push(getImgURLAttrib(e.target)/*e.target.getAttribute("imgURL")*/);
     console.log(`length after: ${favs.length}`);
     e.target.innerHTML = "Favorited!";
