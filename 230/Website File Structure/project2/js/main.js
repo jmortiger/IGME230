4d572e2ef188e33a0b2769f2a360abe1af091596
Justin Morris <jmortiger@gmail.com>
2019-04-11 16:52:46 -0400

    d

--- 230/Website File Structure/project2/js/main.js
+++ 230/Website File Structure/project2/js/main.js
@@ -137,15 +137,16 @@ function jsonLoaded(obj) {
     let bigString;
 
     // Process results
-    //let imgTemplate = `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">Favorite?</button><br /></span></div>`;
+    //let imgTemplate = `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">&hearts;</button><br /></span></div>`;
     if (Array.isArray(obj.message)) {
-        bigString = `<p><i>Here is the result!</i></p><div id="searchResults">`;
+        //bigString = `<p><i>Here is the result!</i></p><div id="searchResults">`;
+        bigString = `<div id="searchResults">`;
         let results = obj.message;
         for (let i = 0; i < results.length; i++) {
             let result = results[i];
             let src = result;
             if (!src) src = "media/no-image-found.png";
-            let line = `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">Favorite?</button><br /></span></div>`;
+            let line = `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">&hearts;</button><br /></span></div>`;
             bigString += line;
         }
         bigString += "</div>";
@@ -155,8 +156,8 @@ function jsonLoaded(obj) {
         let src = obj.message;
         let link = `<a href="${src}">${src}</a>`;
         document.querySelector("#debug").innerHTML += "<br />&nbsp&nbsp&nbsp&nbsp&nbsp<b>Retrieved: </b>" + link;
-        bigString = `<p><i>Here is the result!</i> ${link}</p>`;
-        bigString += `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">Favorite?</button><br /></span></div>`;
+        //bigString = `<p><i>Here is the result!</i> ${link}</p>`;
+        bigString = `<div class="result"><a class="imgLink" href="${src}" target="_blank"><img src="${src}" alt="${src}" /></a><br /><span><button class="favButton" data-img-url="${src}">&hearts;</button><br /></span></div>`;
     }
 
     // 8 - display final results to user
@@ -167,7 +168,8 @@ function jsonLoaded(obj) {
     for (let i = 0; i < favButtons.length; i++) {
         // if the current list of favs has this url
         if (favs.find(function (elem) { return elem == getImgURLAttrib(favButtons[i]); }) != undefined) {
-            favButtons[i].innerHTML = "Favorited!";// TODO: CHANGE CSS TO MAKE IT CLEAR IT'S ALREADY BEEN FAVORITED AND CLICKING WILL UNFAVORITE
+            //favButtons[i].innerHTML = "Favorited!";// TODO: CHANGE CSS TO MAKE IT CLEAR IT'S ALREADY BEEN FAVORITED AND CLICKING WILL UNFAVORITE
+            favButtons[i].style.color = "red";
             favButtons[i].onclick = removeFavorite;
         }
         else
@@ -179,19 +181,24 @@ function addFavorite(e) {
     console.log(`addFavorite called`);
 
     console.log(`length before: ${favs.length}`);
+    //if (getImgURLAttrib(e.target) == "media/no-image-found.png") favs.push(null);
     favs.push(getImgURLAttrib(e.target));
     console.log(`length after: ${favs.length}`);
-    e.target.innerHTML = "Favorited!";
+    //e.target.innerHTML = "Favorited!";
+    e.target.style.color = "red";
     e.target.onclick = removeFavorite;
     storeFavs();
 }
 
 function removeFavorite(e) {
     console.log(`removeFavorite called`);
+
     console.log(`length before: ${favs.length}`);
-    favs = favs.filter((elem) => { return elem != getImgURLAttrib(e.target); });
+    //if (getImgURLAttrib(e.target) == "media/no-image-found.png")
+    favs = favs.filter((elem) => { return elem != getImgURLAttrib(e.target) && elem != null; });
     console.log(`length after: ${favs.length}`);
-    e.target.innerHTML = "Favorite?";
+    //e.target.innerHTML = "Favorite?";
+    e.target.style.color = "black";
     e.target.onclick = addFavorite;
     storeFavs();
 }
@@ -204,7 +211,6 @@ function storeFavs() {
 }
 
 // HELPER FUNCTIONS
-
 // Tries to reformat multi-word breeds into applicable formats (i.e. "golden retriever" -> "retriever-golden")
 function formatUserString(userInput) {
     // Tries to reformat multi-word breeds into applicable formats (i.e. "golden retriever" -> "retriever-golden")
@@ -225,4 +231,4 @@ function getImgURLAttrib(elem) {
     return undefined;
 }
 
-function isntNull(val) { return (favs == null || favs == undefined); }
+function isntNull(val) { return (val == null || val == undefined); }
